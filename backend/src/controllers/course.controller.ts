b07678c5e43a41a9d3f0fa5337cec5a61435c408
siddharth <baghelsiddharth942@gmail.com>
2025-12-05 14:49:31 +0530

    udpate file structure

--- backend/src/controllers/course.controller.ts
+++
@@ -1,8 +0,0 @@
-import type { Request,Response } from "express";
-import { db } from "../config/db.config.ts";
-import { course } from "../models/course.model.ts";
-
-export const courses = async(req:Request,res:Response) => {
-    const allCourses = await db.select().from(course);
-    return res.json({courses: allCourses});
-}
